360347e8286e9b1efe3c0e10288a490482756538
Malthus Oh <malthusoh@gmail.com>
2024-04-12 15:07:18 +0800

    function and file rearrangement

--- app/page.js
+++ app/page.js
@@ -1,68 +1,7 @@
 import "./global.css"
-import Experience from "./experience";
-import Project from "./project";
-
-const getExperiences = () => {
-  return [
-    {
-      title: "Software Engineer Intern",
-      company: "Amplified Industries",
-      location: "Boston, USA",
-      start: "Jul 2023",
-      end: "Jan 2024",
-      techstack: ["Python", "PostgreSQL", "SQL", "AWS S3", "AWS Lambda"]
-    },
-    {
-      title: "Product Manager Intern",
-      company: "Circular",
-      location: "Singapore",
-      start: "May 2023",
-      end: "Jul 2023",
-      techstack: ["Javascript", "Retool", "Airtable", "Figma", "Python", "Dashboarding"]
-    },
-    {
-      title: "Business Analytics Intern",
-      company: "Baxter International Inc",
-      location: "Singapore",
-      start: "May 2021",
-      end: "Jul 2021",
-      techstack: ["Python", "Excel", "Excel VBA", "Tableau"]
-    },
-    {
-      title: "Data Analyst Intern",
-      company: "Wego.com",
-      location: "Singapore",
-      start: "Feb 2020",
-      end: "May 2020",
-      techstack: ["Python", "SQL", "ChartIO", "Google BigQuery"]
-    },
-  ]
-}
-
-const getProjects = () => {
-  return [
-    {
-      title: "ParkWhere: a web app that finds you the best parking spot on-the-go",
-      description: "Loren aoqidoq;dho qofhqw;od qwod;uhqwd ;ouqwhd;qouwd qwd",
-      techstack: ["Vue.js", "HTML", "CSS", "Node", "MongoDB"]
-    },
-    {
-      title: "ParkWhere: an app that finds you the best parking spot in Singapore",
-      description: "Loren aoqidoq;dho qofhqw;od qwod;uhqwd ;ouqwhd;qouwd qwd",
-      techstack: ["Vue.js", "HTML", "CSS", "Node", "MongoDB"]
-    },
-    {
-      title: "ParkWhere: an app that finds you the best parking spot in Singapore",
-      description: "Loren aoqidoq;dho qofhqw;od qwod;uhqwd ;ouqwhd;qouwd qwd",
-      techstack: ["Vue.js", "HTML", "CSS", "Node", "MongoDB"]
-    },
-    {
-      title: "ParkWhere: an app that finds you the best parking spot in Singapore",
-      description: "Loren aoqidoq;dho qofhqw;od qwod;uhqwd ;ouqwhd;qouwd qwd",
-      techstack: ["Vue.js", "HTML", "CSS", "Node", "MongoDB"]
-    },
-  ]
-}
+import Experience from "./components/experience";
+import Project from "./components/project";
+import { getExperiences, getProjects } from "./data";
 
 export default function Content() {
   return (
